81c2393e7f780500604f94b2a4ade952c7f84bb4
PradeepJajjara <jajjarapradeep99@gmail.com>
2021-05-06 20:17:44 +0530

    Added Supplier dataframe and comments

--- contact-supplier-core/main.py
+++ contact-supplier-core/main.py
@@ -10,7 +10,17 @@ from classes import *
 def process(csv_file_path):
     row_limiter = 0
     df = utils.load_suppliers_csv(csv_file_path)
+    '''
+
+    The columns are 
+     - conversation_id(same for the entire thread)
+     - id - message_id
+     - assi_id - assigned_id
+     - is_bot -> Bot if it is bot else Supplier
+     - message -> text
+    '''
     columns = ['cid','id','assi_id','timestamp','is_bot','message','is_reply','thread_id']
+    #create an empty dataframe
     supplier_df = pd.DataFrame(columns = columns)
     suppliers = []
 
@@ -25,8 +35,12 @@ def process(csv_file_path):
 
     utils.multiprocess_supplier_messages(suppliers)
     #print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+
     for supp in suppliers:
         for msg in supp.messages[::-1]:
+            '''
+            Find if the message is end of conversation(by checking for 'actor' attribute)
+            '''
             if hasattr(msg,'actor'):
                 cid = msg.conversationId
                 id_ = msg.id
@@ -36,6 +50,7 @@ def process(csv_file_path):
                 message = msg.eventDescription
                 is_reply = False
                 thread_id = None
+            # for normal messages, get the attributes for dataframe
             else:
                 cid = msg.conversationId
                 id_ = msg.id
@@ -45,10 +60,11 @@ def process(csv_file_path):
                 message = msg.text
                 is_reply = False
                 thread_id = None
-            #print('nn',new_dict)
+            #append the row to the dataframe
             supplier_df = supplier_df.append({'cid':cid,'id':id_,'assi_id': assi_id,'timestamp':timestamp,'is_bot':is_bot,'message':message,'is_reply':is_reply,'thread_id':thread_id},ignore_index  = True)
 
     print(supplier_df)
+    #save it to a csv file
     supplier_df.to_csv('sample1.csv')
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='lets find some suppliers')
